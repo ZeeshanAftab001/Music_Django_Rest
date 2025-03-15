ea04e971f37c308556a5b123a1bb10e70cf7e344
zeeshan aftab <zeeshanaftababbasi@gmail.com>
2025-03-15 12:24:54 +0500

    Done

--- music_backend/settings.py
+++ music_backend/settings.py
@@ -132,11 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 import os
 
-STATIC_DIRS=[os.path.join(BASE_DIR,'music/static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+# MEDIA SETTINGS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STATIC FILES (if needed)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
